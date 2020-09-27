25a1d3f23530096f72c3e4046706578498f24961
SBuchanan1 <syd.buch@gmail.com>
2020-09-27 00:13:49 -0400

    description & table of contents

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,11 +2,18 @@
 function generateMarkdown(data) {
   return `# ${data.title}
   ![<ALT>](https://img.shields.io/badge/${response.label}-${response.message}-${response.color})
+${response.description}
+${response.tableOfContents[0]}
+${response.tableOfContents[1]}
+${response.tableOfContents[2]}
+${response.tableOfContents[3]}
+${response.tableOfContents[4]}
+${response.tableOfContents[5]}
 
 `
+  ${ response.description }
 
-
-    ; { type: inputs }
+  ; { type: inputs }
 }
 
 module.exports = generateMarkdown;
