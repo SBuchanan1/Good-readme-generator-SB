67ee39f38aa4b1ecabd3166ddc3b45e41ff4ee6d
SBuchanan1 <syd.buch@gmail.com>
2020-11-10 04:46:23 -0500

    changed badge color to purple

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,33 +1,81 @@
-// const licenseBadges = require("./licenseBadges");
-
-// Function to Generate README
-
-function documentGenerator(response) {
-  return `#${response.title}
-  
-
-  ## Description
-  ${response.description}
-
-
-
-  ##Installation
-  ${response.installation}
-
-  ##Usage
-  ${response.usage}
-
-  ##Contributors
-  ${response.contributors}
-
-  ##Tests
-  ${response.tests}
-
-  ##Questions
-  Please reach out to Sydney Buchanan for further questions
-
-
-` ;
-
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-purple.svg)`
+  }
+  return ''
 }
+
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return (
+      `\n* [License](#license)\n`
+    )
+  }
+  return ''
+}
+
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return (
+      `## License
+
+This project is licensed under the ${license} license.`
+    )
+  }
+  return ''
+}
+
+function generateMarkdown(data) {
+  return `# ${data.title}
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+${renderLicenseLink(data.license)}
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+  
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+To run tests, run the following command:
+
+\`\`\`
+${data.test}
+\`\`\`
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}/).
+
+`;
+}
+
 module.exports = generateMarkdown;
